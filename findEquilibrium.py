b85a208818905ca976d77823633c63156c0cd4eb
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-01-14 08:34:10 +0530

    Create findEquilibrium.py

---
+++ findEquilibrium.py
@@ -0,0 +1,28 @@
+"""
+Given an array arr[] of non-negative numbers. The task is to find the first equilibrium point in the array.
+
+The equilibrium point in an array is an index (0-based indexing) such that the sum of all elements before that index is the same as the sum of elements after it. Return -1 if no such point exists. 
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 3]
+Output: 2 
+Explanation: The sum of left of index 2 is 1 + 2 = 3 and sum on right of index 2 is 0 + 3 = 3.
+Input: arr[] = [1, 1, 1, 1]
+Output: -1
+Explanation: There is no equilibrium index in the array.
+Input: arr[] = [-7, 1, 5, 2, -4, 3, 0]
+Output: 3
+Explanation: The sum of left of index 3 is -7 + 1 + 5 = -1 and sum on right of index 3 is -4 + 3 + 0 = -1.
+Constraints:
+3 <= arr.size() <= 106
+0 <= arr[i] <= 109
+"""
+def findEquilibrium(self, arr):
+        total_sum = sum(arr)
+        left_sum = 0
+        for i in range(len(arr)):
+            if left_sum == total_sum - arr[i] - left_sum :
+                return i
+            left_sum += arr[i]
+        return -1
