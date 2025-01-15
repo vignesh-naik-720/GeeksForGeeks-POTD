095e5d68770f9564b5cfb54bfce72b5a2ab33152
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-01-15 22:06:00 +0530

    Update longestSubarray.py

--- longestSubarray.py
+++ longestSubarray.py
@@ -21,20 +21,15 @@ def longestSubarray(self, arr, k):
         prefix_sum = {}
         current_sum = 0
         max_length = 0
-    
         for i in range(len(arr)):
             current_sum += arr[i]
-    
             # Check if the sum of the subarray from the start equals k
             if current_sum == k:
                 max_length = i + 1
-    
             # Check if there is a prefix_sum that satisfies the condition
             if (current_sum - k) in prefix_sum:
                 max_length = max(max_length, i - prefix_sum[current_sum - k])
-    
             # Store current_sum in the dictionary if it is not already stored
             if current_sum not in prefix_sum:
                 prefix_sum[current_sum] = i
-    
         return max_length
