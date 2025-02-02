cbedfbfdf6fdb5d15fb466f356a16b797c60da3e
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-02-02 22:38:54 +0530

    Update levelOrder.py

--- levelOrder.py
+++ levelOrder.py
@@ -6,10 +6,8 @@ Output: [[1], [2, 3]]
 Input: root[] = [10, 20, 30, 40, 50]
 Output: [[10], [20, 30], [40, 50]]
 Input: root[] = [1, 3, 2, N, N, N, 4, 6, 5]
-
 Output: [[1], [3, 2], [4], [6, 5]]
 Constraints:
-
 1 ≤ number of nodes ≤ 10^5
 0 ≤ node->data ≤ 10^9
 """
