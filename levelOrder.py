7ea63f955ef1af84033c07ad2d03d66556c1158d
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-02-02 22:38:23 +0530

    Update levelOrder.py

--- levelOrder.py
+++ levelOrder.py
@@ -2,7 +2,6 @@
 Given a root of a binary tree with n nodes, the task is to find its level order traversal. Level order traversal of a tree is breadth-first traversal for the tree.
 
 Examples:
-
 Input: root[] = [1, 2, 3]
 
 Output: [[1], [2, 3]]
