b945f182d70e8f2f55371126fbc049e45d767ce8
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-02-04 22:09:15 +0530

    Update diameter.py

--- diameter.py
+++ diameter.py
@@ -13,19 +13,14 @@ Constraints:
 """
 def diameter(self, root):
         self.max_diameter = 0
-
         def depth(node):
             if not node:
                 return 0
-            
             left_depth = depth(node.left)
             right_depth = depth(node.right)
-            
             # Update the maximum diameter found so far
             self.max_diameter = max(self.max_diameter, left_depth + right_depth)
-            
             # Return the height of the current node
             return 1 + max(left_depth, right_depth)
-        
         depth(root)
         return self.max_diameter
