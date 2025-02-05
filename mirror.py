4f3bbc1b2cff1668efbbd1be83ce22881b7fff11
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-02-05 23:29:41 +0530

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -1,6 +1,5 @@
 """
 Given a binary tree, convert the binary tree to its Mirror tree.
-
 Mirror of a Binary Tree T is another Binary Tree M(T) with left and right children of all non-leaf nodes interchanged.     
 
 Examples:
