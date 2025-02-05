8b7efc4e109177f25b7bd15191c1dc607c66d420
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-02-05 23:30:04 +0530

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -5,7 +5,6 @@ Examples:
 Input: root[] = [1, 2, 3, N, N, 4]
 Output: [1, 3, 2, N, 4]
 Explanation: 
-
 In the inverted tree, every non-leaf node has its left and right child interchanged.
 Input: root[] = [1, 2, 3, 4, 5]
 Output: [1, 3, 2, N, N, 5, 4]
