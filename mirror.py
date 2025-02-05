cc09c7327a5f117d6ba0f2ce6155878a611e5af0
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-02-05 23:31:00 +0530

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -11,8 +11,8 @@ Output: [1, 3, 2, N, N, 5, 4]
 Explanation:
 In the inverted tree, every non-leaf node has its left and right child interchanged.
 Constraints:
-1 ≤ number of nodes ≤ 105
-1 ≤ node->data ≤ 105
+1 ≤ number of nodes ≤ 10^5
+1 ≤ node->data ≤ 10^5
 """
 def mirror(self, root):
         if root is None:
