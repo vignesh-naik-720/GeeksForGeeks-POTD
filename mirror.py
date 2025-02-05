e3b5a7ad4bfbce806390ad5b1f6941089f19b7eb
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2025-02-05 23:30:11 +0530

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -9,7 +9,6 @@ In the inverted tree, every non-leaf node has its left and right child interchan
 Input: root[] = [1, 2, 3, 4, 5]
 Output: [1, 3, 2, N, N, 5, 4]
 Explanation:
-
 In the inverted tree, every non-leaf node has its left and right child interchanged.
 Constraints:
 1 ≤ number of nodes ≤ 105
